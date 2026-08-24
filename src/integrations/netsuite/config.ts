4100ffc25df95ca782ae414f3100f2fb577efa4f
marktapia-paragon <298055460+marktapia-paragon@users.noreply.github.com>
2026-08-24 21:53:10 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/netsuite/config.ts
@@ -0,0 +1,18 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a netsuite
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync purchase orders with NetSuite',
+  overviewText: `Connect to your NetSuite ERP system to manage your vendors and purchase orders in NetSuite. Increase your team’s productivity by keeping your NetSuite ERP system up to date - without manual data entry.
+    
+Our NetSuite integration enables you to:
+  
+• Create or update purchase orders in NetSuite automatically
+• Sync vendor information between your app and NetSuite`,
+  showWatermark: false,
+  workflowDisplayOrder: [],
+};
+
+export default config;
