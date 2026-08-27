d73fd18056eb5f37234ab95e3ab49c3b92ddaa71
marktapia-paragon <298055460+marktapia-paragon@users.noreply.github.com>
2026-08-27 22:54:49 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/sharepoint/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a sharepoint
+ */
+const config: IIntegrationConfig = {
+  description: 'Manage sites and lists in SharePoint',
+  overviewText: `Connect to your SharePoint account to manage sites and lists in SharePoint. Increase your team’s productivity by keeping your SharePoint sites up to date - without manual data entry.
+  
+Our SharePoint integration enables you to:
+  
+• Automatically create or update lists and items in SharePoint
+• Sync lists and items from SharePoint
+• Receive updates when lists and items are created or updated in SharePoint`,
+  showWatermark: false,
+  workflowDisplayOrder: [],
+};
+
+export default config;
